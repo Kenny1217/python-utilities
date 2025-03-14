b16b76a583dff6d6e903a17551fb756dfb067c8f
Kenny Morris <kmorris1217@gmail.com>
2025-03-13 23:34:38 -0400

    start wrapper for postgresql

---
+++ sql-utilities/postgresql/postgresql_database.py
@@ -0,0 +1,33 @@
+import psycopg2
+from psycopg2 import OperationalError
+
+
+class PostgreSQLDatabase:
+    def __init__(self, dbname, host, port, user, password):
+        self.dbname = dbname
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            print(f"Attempting to connect to {self.dbname} at {self.host}:{self.port}")
+            self.connection = psycopg2.connect(
+                database = self.dbname,
+                user = self.user,
+                password = self.password,
+                host = self.host,
+                port = self.port,
+            )
+            self.cursor = self.connection.cursor()
+            print(f"Connected to {self.dbname}")
+        except OperationalError as e:
+            print(f"Error: Unable to connect to the database: {e}")
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close
+        print(f"Connection closed")
